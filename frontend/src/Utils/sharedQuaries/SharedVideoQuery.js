b599a53bcb11f2e2c5195778489dce7dd150c5db
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-05 23:59:39 +0530

    Refactor: Update API endpoint in SharedVideoQuery.js

--- frontend/src/Utils/sharedQuaries/SharedVideoQuery.js
+++ frontend/src/Utils/sharedQuaries/SharedVideoQuery.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 const fetchVideos = () => {
-    return axios.get("/api/videos");
+    return axios.get("https://video-tube-eight.vercel.app/api/videos");
 }
 
 export const useSharedVideoQuery = () => {
