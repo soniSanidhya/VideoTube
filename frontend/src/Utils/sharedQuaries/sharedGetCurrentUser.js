87f81a032857895ee42d58141ea0de86f034b1e8
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-06 21:23:55 +0530

    Refactor: Add withCredentials option to getCurrentUser API call

--- frontend/src/Utils/sharedQuaries/sharedGetCurrentUser.js
+++ frontend/src/Utils/sharedQuaries/sharedGetCurrentUser.js
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const getCurrentUser = () => axios.get("https://video-tube-eight.vercel.app/api/users/current-user");
+const getCurrentUser = () => axios.get("https://video-tube-eight.vercel.app/api/users/current-user" , {
+  withCredentials: true,
+});
 
 export const useGetCurrentUser = (isLogin) => useQuery({
     queryKey: ["currentUser"],
