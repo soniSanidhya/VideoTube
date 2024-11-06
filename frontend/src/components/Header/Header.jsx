0b16589aa19ddf478b66498d4d332603d4b35167
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-06 23:25:21 +0530

    Refactor: Update header layout and styling for improved responsiveness and user experience

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -312,7 +312,7 @@ const Header = () => {
             </li>
           </ul>
           {isLogin ? (
-            <div className="flex gap-4">
+            <div className="mb-8 mt-auto flex w-full flex-wrap gap-4 px-4 sm:mb-0 sm:mt-0 sm:items-center sm:px-0">
               <Link to={`/channel/c/${data?.data.data.username}`}>
                 <div className="mb-8 mt-auto px-4 sm:mb-0 sm:mt-0 sm:px-0">
                   <button className="flex w-full gap-4 text-left sm:items-center">
@@ -330,10 +330,8 @@ const Header = () => {
                 </div>
               </Link>
               {/* <div className="mb-8 mt-auto flex w-full flex-wrap gap-4 px-4 sm:mb-0 sm:mt-0 sm:items-center sm:px-0"> */}
-                <button
-                  onClick={handleLogout}
-                  className="mr-1 w-full bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto"
-                >
+              
+              <button  onClick={handleLogout} className="w-full bg-[#383737] px-3 py-2 hover:bg-[#4f4e4e] sm:w-auto sm:bg-transparent">
                   Log Out
                 </button>
               {/* </div> */}
