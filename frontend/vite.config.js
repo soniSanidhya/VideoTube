d3191fc728376b622ce855ab3ebb61c9848c9636
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-06 19:52:26 +0530

    Refactor: Update API request in SharedVideoQuery.js and enhance proxy configuration in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api': {
-      //   target: 'https://video-tube-eight.vercel.app', // Your backend server URL
-      //   changeOrigin: true, // Changes the origin of the host header to the target URL
-      //   rewrite: (path) => path.replace(/^\/api/, ''), // Optional: rewrite path
-      // }
-
-      '/api' : 'https://video-tube-eight.vercel.app'
+      '/api': {
+        target: 'https://video-tube-eight.vercel.app',
+        changeOrigin: true,  // Ensures correct request origin
+        secure: true         // Ensures HTTPS
+      }
     }
   }
 })
