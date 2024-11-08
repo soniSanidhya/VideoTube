f86e2684fd84f7f36f67bb40d7533106bc9eb17c
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-08 20:53:18 +0530

    Refactor: Ensure temporary directory exists in multer middleware before file upload

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -6,7 +6,12 @@ const storage = multer.diskStorage(
         destination : function (req , file , cb) {
             console.log("multter middleware called");
             
-            cb(null , "./public/temp");
+            const fs = require('fs');
+            const dir = './public/temp';
+            if (!fs.existsSync(dir)){
+                fs.mkdirSync(dir, { recursive: true });
+            }
+            cb(null , dir);
         },
         filename : function (req , file ,cb) {
             cb(null , file.originalname);
