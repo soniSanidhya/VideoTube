3d10f98fcf8c3fea0ce8fa4de0397f5133229e2f
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-09 21:23:47 +0530

    Refactor: Clean up video upload middleware by removing commented code and adjusting logging for Cloudinary configuration

--- backend/src/middleware/videoUploadMulter.js
+++ backend/src/middleware/videoUploadMulter.js
@@ -8,7 +8,6 @@
 // // Configure Cloudinary
 // const cloudinary = cloudinaryModule.v2;
 
-
 // cloudinary.config({
 //     cloud_name: process.env.CLOUD_NAME,
 //     api_key: process.env.API_KEY,
@@ -29,11 +28,10 @@
 
 // export { upload };
 
-
-import cloudinary from 'cloudinary';
-import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import dotenv from 'dotenv';
+import cloudinary from "cloudinary";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -44,24 +42,26 @@ cloudinary.config({
     api_secret: process.env.API_SECRET_KEY,
 });
 
-console.log('cloudinary video multer');
-
+// console.log('cloudinary video multer');
 
 // Configure Cloudinary storage for Multer
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    resource_type: 'auto',  // Matches the previous setting for dynamic file types
-    public_id: (req, file) => file.originalname.split('.')[0],  // Retain original file name as public ID
-    // Optional: you could add folder here if needed
-  },
+    cloudinary: cloudinary,
+    params: {
+        resource_type: "auto", // Matches the previous setting for dynamic file types
+        public_id: (req, file) => {
+            console.log("videomulter cloudinary");
+            
+            return file.originalname.split(".")[0];
+        }, // Retain original file name as public ID
+        // Optional: you could add folder here if needed
+    },
 });
 
 // const upload = multer({ storage } , );
 const upload = multer({
     storage,
     limits: { fileSize: 100 * 1024 * 1024 }, // Set limit to 50 MB
-  });
-  
+});
 
 export { upload };
