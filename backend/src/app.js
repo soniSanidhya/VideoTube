f75b962e06d9f3878bbaa8742eb2f73020c7126d
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-09 21:02:25 +0530

    Refactor: Increase JSON and URL-encoded body size limits to 100mb in app configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,8 @@ app.use(cors({
     origin: 'https://videotube-one.vercel.app',
     credentials: true
 }));
-app.use(express.json({limit : "16kb"}));
-app.use(express.urlencoded({extended : true, limit : "16kb" }));
+app.use(express.json({limit : "100mb"}));
+app.use(express.urlencoded({extended : true, limit : "100mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
