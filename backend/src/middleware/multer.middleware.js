3fc6fc2f31b64aad08141430fd2c8a362228a3cc
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-12 15:17:32 +0530

    Refactor: Increase file upload size limit to 100MB and update Vercel configuration for function memory and duration

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -84,6 +84,6 @@ const storage = multer.memoryStorage();
 export const upload = multer({ 
     storage,
     limits: {
-        fileSize: 100 * 1024 * 1024, // 5MB limit
+        fileSize: 100 * 1024 * 1024, // 100MB limit
     }
 });
