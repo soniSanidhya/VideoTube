2a8bec4e33e24cf58b662cd74a3926476627925a
Sanidhya Soni <sanidhyasn314@gmail.com>
2024-11-12 15:03:33 +0530

    Refactor: Simplify video upload UI in VideoModelPopUp by restructuring conditional rendering and improving layout

--- frontend/src/components/Channel/VideoModelPopUp.jsx
+++ frontend/src/components/Channel/VideoModelPopUp.jsx
@@ -31,7 +31,6 @@ const VideoModelPopUp = (uploaded) => {
   };
 
   console.log("video", fileURL);
-  
 
   const { mutate, isPending, isSuccess } = useMutation({
     mutationFn: (form) => postVideo(form),
@@ -280,52 +279,51 @@ const VideoModelPopUp = (uploaded) => {
             </button>
           </div>
           <div className="mx-auto flex w-full max-w-3xl flex-col gap-y-4 p-4">
-            {video ? (
-               <video className="h-full w-full" controls autoPlay muted>
-               <source
-                 src={fileURL} 
-                 type="video/mp4"
-               />
-             </video>
-           
-            ) : (
-              <div className="w-full border-2 border-dashed px-4 py-12 text-center">
-                <span className="mb-4 inline-block w-24 rounded-full bg-[#E4D3FF] p-4 text-[#AE7AFF]">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                    ></path>
-                  </svg>
-                </span>
-                <h6 className="mb-2 font-semibold">
-                  Drag and drop video files to upload
-                </h6>
-                <p className="text-gray-400">
-                  Your videos will be private untill you publish them.
-                </p>
-                <label
-                  htmlFor="upload-video"
-                  className="group/btn mt-4 inline-flex w-auto cursor-pointer items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]"
-                >
-                  <input
-                    onChange={handleVideo}
-                    type="file"
-                    id="upload-video"
-                    className="sr-only"
-                  />
-                  Select Files
-                </label>
-              </div>
-            )}
+            <div className="w-full border-2 border-dashed px-4 py-12 text-center">
+              {video ? (
+                <video className="h-full w-full" controls autoPlay muted>
+                  <source src={fileURL} type="video/mp4" />
+                </video>
+              ) : (
+                <div>
+                  <span className="mb-4 inline-block w-24 rounded-full bg-[#E4D3FF] p-4 text-[#AE7AFF]">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                      ></path>
+                    </svg>
+                  </span>
+                  <h6 className="mb-2 font-semibold">
+                    Drag and drop video files to upload
+                  </h6>
+                  <p className="text-gray-400">
+                    Your videos will be private untill you publish them.
+                  </p>
+                </div>
+              )}
+              <label
+                htmlFor="upload-video"
+                className="group/btn mt-4 inline-flex w-auto cursor-pointer items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]"
+              >
+                <input
+                  onChange={handleVideo}
+                  type="file"
+                  id="upload-video"
+                  className="sr-only"
+                />
+                Select Files
+              </label>
+            </div>
+
             <div className="w-full">
               <label htmlFor="thumbnail" className="mb-1 inline-block">
                 Thumbnail
